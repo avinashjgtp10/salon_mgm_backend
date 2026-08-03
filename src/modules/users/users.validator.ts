c481c7f8dff65ce4b845ddbe487d6e1820707203
shivani <shivanidipak122004@gmail.com>
2026-08-03 10:41:47 +0530

    SCRUM-1242: Allow password update even if strength is weak

--- src/modules/users/users.validator.ts
+++ src/modules/users/users.validator.ts
@@ -82,7 +82,6 @@ export const changePasswordSchema = z.object({
     .string()
     .min(8, "Password must be at least 8 characters")
     .regex(/[A-Z]/, "Must contain at least one uppercase letter")
-    .regex(/[a-z]/, "Must contain at least one lowercase letter")
     .regex(/[0-9]/, "Must contain at least one number"),
 });
 
